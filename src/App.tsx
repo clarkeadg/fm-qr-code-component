abd6464dc82b4ce1f2c6003534dae8f252791496
clarkeadg <clarkeadg@yahoo.com>
2023-12-18 20:48:47 -0800

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import QRCode from './components/QRCode'
 
 const App = () => {
   return (
-    <main className="bg-app relative flex min-h-screen justify-center items-center py-10">
+    <main className="relative flex min-h-screen justify-center items-center py-10">
       <QRCode/>
       <div className="absolute bottom-0 w-full h-10 flex items-center justify-center">
         <Attribution/>
