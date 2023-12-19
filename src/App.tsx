4150d420fadd32bc1b13504270a1fa74e5d1fa9d
clarkeadg <clarkeadg@yahoo.com>
2023-12-18 19:09:25 -0800

    accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import QRCode from './components/QRCode'
 
 const App = () => {
   return (
-    <div className="bg-app relative flex min-h-screen justify-center items-center py-10">
+    <main className="bg-app relative flex min-h-screen justify-center items-center py-10">
       <QRCode/>
       <div className="absolute bottom-0 w-full h-10 flex items-center justify-center">
         <Attribution/>
       </div>
-    </div>
+    </main>
   )
 }
 
--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -7,9 +7,9 @@ const QRCode = () => {
         <img className="w-full" src={imgUrl} alt="QR Code"/>
       </div>      
       <div className="px-4 py-6">
-        <div className="text-dark font-bold text-2xl leading-tight mb-4">
+        <h1 className="text-dark font-bold text-2xl leading-tight mb-4">
           Improve your front-end skills by building projects
-        </div>
+        </h1>
         <p className="text-light leading-tight">
           Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
         </p>
